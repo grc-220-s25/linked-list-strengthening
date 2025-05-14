94fc2044001ddee1e336f3e9db4130d0f46bea88
Johncarlo Perez <johncarloperez@Johncarlos-MacBook-Pro.local>
2025-05-14 14:30:50 -0700

    finished removeLast method

--- src/Exercises.java
+++ src/Exercises.java
@@ -54,9 +54,17 @@ public class Exercises {
      * @return the head of the list with the last element removed
      */
     public static ListNode removeLast(ListNode head) {
-        if(head == null || head.next == null) return null;
-
-
+        
+        if(head == null || head.next == null) {
+            return null;
+        }
+       
+        ListNode current = head;
+        while(current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+        return head;
     }
 
     /**
@@ -71,7 +79,8 @@ public class Exercises {
      * @return the minimum value in the list 
      */
     public static int min(ListNode head) {
-        return -1;
+       
+        return currentSmallest;
     }
 
     /**
