c312ca2ab6c03b294fe0f7b317fd8753a6ae4ad7
Kelley Castillo <kelleycastillo@Kelleys-MacBook-Pro.local>
2025-05-18 18:43:56 -0700

    finished removeLast

--- src/Exercises.java
+++ src/Exercises.java
@@ -54,7 +54,15 @@ public class Exercises {
      * @return the head of the list with the last element removed
      */
     public static ListNode removeLast(ListNode head) {
+        if (head == null || head.next == null) {
         return null;
+    }
+        ListNode current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+        return head;
     }
 
     /**
